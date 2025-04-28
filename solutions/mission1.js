c4ae71b9b9401d3838329418328f038ad8a41c10
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-04-29 00:22:12 +0200

    feat: add solution for mission 1

---
+++ solutions/mission1.js
@@ -0,0 +1,4 @@
+// Installer nodeJS depuis https://nodejs.org/fr/download
+// Puis ouvrez le terminal et exécutez la commande suivante pour vérifier l'installation:
+// node -v
+// Si une version de node apparait, c'est que l'installation a réussi
