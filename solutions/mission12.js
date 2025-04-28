3a195302e477a95f651055e685c14d70dce19a3d
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-04-29 00:42:20 +0200

    feat: draft 2 for solution

--- solutions/mission12.js
+++ solutions/mission12.js
@@ -1,22 +1,29 @@
 // En cours de développement. Proposer une solution via une pull request sur le dépôt GitHub du projet.
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
 class Hero {
-    constructor(name, healPoints, attackPoints) {
+    constructor(name) {
         this.name = name;
-        this.healPoints = healPoints;
-        this.attackPoints = attackPoints;
+        this.healPoints = getRandomArbitrary(50, 100).toFixed(0);
+        this.attackPoints = getRandomArbitrary(5, 20).toFixed(0);
     }
 }
 
 class Monster {
-    constructor(name, healPoints, attackPoints) {
+    constructor(name) {
         this.name = name;
-        this.healPoints = healPoints;
-        this.attackPoints = attackPoints;
+        this.healPoints = getRandomArbitrary(50, 100).toFixed(0);
+        this.attackPoints = getRandomArbitrary(5, 20).toFixed(0);
     }
 }
 
-let hero = new Hero("Superman", 10, 8);
-let monster = new Monster("Godzilla", 12, 6);
+let hero = new Hero("Superman");
+let monster = new Monster("Godzilla");
 
 console.log(`${hero.name} vs ${monster.name}`);
+console.log(`Hero: ${hero.healPoints} HP, ${hero.attackPoints} ATK`);
+console.log(`Monster: ${monster.healPoints} HP, ${monster.attackPoints} ATK`);
+console.log("Battle begins!");
