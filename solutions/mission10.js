6fa7c5a301b264b4aefcd5f2c4e03f12a1c56587
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-04-29 00:16:03 +0200

    feat: solution for mission 10

--- solutions/mission10.js
+++ solutions/mission10.js
@@ -1,1 +1,19 @@
-// En cours de développement. Proposer une solution via une pull request sur le dépôt GitHub du projet.
+async function getProducts() {
+  const response = await fetch('https://fakestoreapi.com/products');
+  const data = await response.json();
+  return data;
+}
+
+let products = await getProducts();
+
+let cheapestPrice = products[0].price;
+let cheapestProduct = products[0];
+
+products.forEach(element => {
+    if (element.price < cheapestPrice) {
+        cheapestPrice = element.price;
+        cheapestProduct = element;
+    }
+});
+
+console.log(`Le produit le moins cher est : ${cheapestProduct.title}`);
