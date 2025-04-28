4148d8a776788166b71d053765ff03c3e92c9e2a
Frédéric Bui <bui.frederic.pro@gmail.com>
2025-04-29 00:25:32 +0200

    feat: add solution for mission 11

--- solutions/mission11.js
+++ solutions/mission11.js
@@ -1,1 +1,11 @@
-// En cours de développement. Proposer une solution via une pull request sur le dépôt GitHub du projet.
+async function getProducts() {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    return data;
+}
+
+let products = await getProducts();
+
+let fourStars = products.filter(product => product.rating.rate >= 4);
+
+console.log(fourStars);
